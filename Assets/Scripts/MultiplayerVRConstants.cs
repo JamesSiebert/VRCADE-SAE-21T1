0c8dee9189f98935ccc48d674ff16464c2d76bea
James Siebert <js@media360.com.au>
2021-02-24 12:04:13 +1100

    updated room manager script

--- Assets/Scripts/MultiplayerVRConstants.cs
+++ Assets/Scripts/MultiplayerVRConstants.cs
@@ -6,6 +6,13 @@ public class MultiplayerVRConstants
     public const string MAP_TYPE_SCHOOL = "school";
     public const string MAPT_TYPE_OUTDOOR = "outdoor";
 
+    public const string MAP_TYPE_LOBBY = "lobby";
+    public const string MAP_TYPE_VERTIGO = "vertigo";
+    public const string MAP_TYPE_AIR_HOCKEY = "air_hockey";
+    public const string MAP_TYPE_BASKETBALL = "basketball";
+    public const string MAP_TYPE_ARCHERY = "archery";
+    public const string MAP_TYPE_SHOOTER = "shooter";
+
 
     public const string AVATAR_SELECTION_NUMBER = "Avatar_Selection_Number";
 }
--- Assets/Scripts/RoomManager.cs
+++ Assets/Scripts/RoomManager.cs
@@ -12,6 +12,24 @@ public class RoomManager : MonoBehaviourPunCallbacks
 
     [SerializeField]
     TextMeshProUGUI OccupancyRateText_ForOutdoor;
+
+    [SerializeField]
+    TextMeshProUGUI OccupancyRateText_ForLobby;
+
+    [SerializeField]
+    TextMeshProUGUI OccupancyRateText_ForVertigo;
+
+    [SerializeField]
+    TextMeshProUGUI OccupancyRateText_ForAirHockey;
+
+    [SerializeField]
+    TextMeshProUGUI OccupancyRateText_ForBasketball;
+
+    [SerializeField]
+    TextMeshProUGUI OccupancyRateText_ForArchery;
+
+    [SerializeField]
+    TextMeshProUGUI OccupancyRateText_ForShooter;
     string mapType;
 
     void Start()
@@ -56,7 +74,47 @@ public class RoomManager : MonoBehaviourPunCallbacks
         PhotonNetwork.JoinRandomRoom(expectedCustomRoomProperties, 0);
     }
 
+    public void OnEnterRoomButtonClicked_Lobby()
+    {
+        mapType = MultiplayerVRConstants.MAP_TYPE_LOBBY;
+        ExitGames.Client.Photon.Hashtable expectedCustomRoomProperties = new ExitGames.Client.Photon.Hashtable() { {MultiplayerVRConstants.MAP_TYPE_KEY, mapType} };
+        PhotonNetwork.JoinRandomRoom(expectedCustomRoomProperties, 0);
+    }
 
+    public void OnEnterRoomButtonClicked_Vertigo()
+    {
+        mapType = MultiplayerVRConstants.MAP_TYPE_VERTIGO;
+        ExitGames.Client.Photon.Hashtable expectedCustomRoomProperties = new ExitGames.Client.Photon.Hashtable() { {MultiplayerVRConstants.MAP_TYPE_KEY, mapType} };
+        PhotonNetwork.JoinRandomRoom(expectedCustomRoomProperties, 0);
+    }
+
+    public void OnEnterRoomButtonClicked_AirHockey()
+    {
+        mapType = MultiplayerVRConstants.MAP_TYPE_AIR_HOCKEY
+        ExitGames.Client.Photon.Hashtable expectedCustomRoomProperties = new ExitGames.Client.Photon.Hashtable() { {MultiplayerVRConstants.MAP_TYPE_KEY, mapType} };
+        PhotonNetwork.JoinRandomRoom(expectedCustomRoomProperties, 0);
+    }
+
+    public void OnEnterRoomButtonClicked_Basketball()
+    {
+        mapType = MultiplayerVRConstants.MAP_TYPE_BASKETBALL;
+        ExitGames.Client.Photon.Hashtable expectedCustomRoomProperties = new ExitGames.Client.Photon.Hashtable() { {MultiplayerVRConstants.MAP_TYPE_KEY, mapType} };
+        PhotonNetwork.JoinRandomRoom(expectedCustomRoomProperties, 0);
+    }
+
+    public void OnEnterRoomButtonClicked_Archery()
+    {
+        mapType = MultiplayerVRConstants.MAP_TYPE_ARCHERY;
+        ExitGames.Client.Photon.Hashtable expectedCustomRoomProperties = new ExitGames.Client.Photon.Hashtable() { {MultiplayerVRConstants.MAP_TYPE_KEY, mapType} };
+        PhotonNetwork.JoinRandomRoom(expectedCustomRoomProperties, 0);
+    }
+
+    public void OnEnterRoomButtonClicked_Shooter()
+    {
+        mapType = MultiplayerVRConstants.MAP_TYPE_SHOOTER;
+        ExitGames.Client.Photon.Hashtable expectedCustomRoomProperties = new ExitGames.Client.Photon.Hashtable() { {MultiplayerVRConstants.MAP_TYPE_KEY, mapType} };
+        PhotonNetwork.JoinRandomRoom(expectedCustomRoomProperties, 0);
+    }
 
 
     #endregion
@@ -93,12 +151,41 @@ public class RoomManager : MonoBehaviourPunCallbacks
                 {
                     // Load school scene
                     PhotonNetwork.LoadLevel("World_School");
-
                 } 
                 else if ((string)mapType == MultiplayerVRConstants.MAPT_TYPE_OUTDOOR)
                 {
                     // Load outdoor scene
                     PhotonNetwork.LoadLevel("World_Outdoor");
+                }
+                else if ((string)mapType == MultiplayerVRConstants.MAP_TYPE_LOBBY)
+                {
+                    // Load outdoor scene
+                    PhotonNetwork.LoadLevel("Room_Lobby");
+                }
+                else if ((string)mapType == MultiplayerVRConstants.MAP_TYPE_VERTIGO)
+                {
+                    // Load outdoor scene
+                    PhotonNetwork.LoadLevel("Room_Vertigo");
+                }
+                else if ((string)mapType == MultiplayerVRConstants.MAP_TYPE_AIR_HOCKEY)
+                {
+                    // Load outdoor scene
+                    PhotonNetwork.LoadLevel("Room_AirHockey");
+                }
+                else if ((string)mapType == MultiplayerVRConstants.MAP_TYPE_BASKETBALL)
+                {
+                    // Load outdoor scene
+                    PhotonNetwork.LoadLevel("Room_Basketball");
+                }
+                else if ((string)mapType == MultiplayerVRConstants.MAP_TYPE_ARCHERY)
+                {
+                    // Load outdoor scene
+                    PhotonNetwork.LoadLevel("Room_Archery");
+                }
+                else if ((string)mapType == MultiplayerVRConstants.MAP_TYPE_SHOOTER)
+                {
+                    // Load outdoor scene
+                    PhotonNetwork.LoadLevel("Room_Shooter");
                 }
             }
         }
@@ -119,11 +206,19 @@ public class RoomManager : MonoBehaviourPunCallbacks
             // No Room
             OccupancyRateText_ForSchool.text = 0 + " / " + 20;
             OccupancyRateText_ForOutdoor.text = 0 + " / " + 20;
+
+            OccupancyRateText_ForLobby.text = 0 + " / " + 20;
+            OccupancyRateText_ForVertigo.text = 0 + " / " + 20;
+            OccupancyRateText_ForAirHockey.text = 0 + " / " + 20;
+            OccupancyRateText_ForBasketball.text = 0 + " / " + 20;
+            OccupancyRateText_ForArchery.text = 0 + " / " + 20;
+            OccupancyRateText_ForShooter.text = 0 + " / " + 20;
         }
 
         foreach(RoomInfo room in roomList)
         {
             Debug.Log(room.Name);
+
             if(room.Name.Contains(MultiplayerVRConstants.MAPT_TYPE_OUTDOOR))
             {
                 // Update the outdoor room map
@@ -136,8 +231,39 @@ public class RoomManager : MonoBehaviourPunCallbacks
                 Debug.Log("Room is a SCHOOL map. Player count is: " + room.PlayerCount);
                 OccupancyRateText_ForSchool.text = room.PlayerCount + " / " + 20;
             }
+            else if (room.Name.Contains(MultiplayerVRConstants.MAP_TYPE_LOBBY))
+            {
+                Debug.Log("Room is a LOBBY map. Player count is: " + room.PlayerCount);
+                OccupancyRateText_ForLobby.text = room.PlayerCount + " / " + 20;
+            }
+            else if (room.Name.Contains(MultiplayerVRConstants.MAP_TYPE_VERTIGO))
+            {
+                Debug.Log("Room is a VERTIGO map. Player count is: " + room.PlayerCount);
+                OccupancyRateText_ForVertigo.text = room.PlayerCount + " / " + 20;
+            }
+            else if (room.Name.Contains(MultiplayerVRConstants.MAP_TYPE_AIR_HOCKEY))
+            {
+                Debug.Log("Room is a AIR HOCKEY map. Player count is: " + room.PlayerCount);
+                OccupancyRateText_ForAirHockey.text = room.PlayerCount + " / " + 20;
+            }
+            else if (room.Name.Contains(MultiplayerVRConstants.MAP_TYPE_BASKETBALL))
+            {
+                Debug.Log("Room is a BASKETBALL map. Player count is: " + room.PlayerCount);
+                OccupancyRateText_ForBasketball.text = room.PlayerCount + " / " + 20;
+            }
+            else if (room.Name.Contains(MultiplayerVRConstants.MAP_TYPE_ARCHERY))
+            {
+                Debug.Log("Room is a ARCHERY map. Player count is: " + room.PlayerCount);
+                OccupancyRateText_ForArchery.text = room.PlayerCount + " / " + 20;
+            }
+            else if (room.Name.Contains(MultiplayerVRConstants.MAP_TYPE_SCHOOL))
+            {
+                // Update the school room map
+                Debug.Log("Room is a SHOOTER map. Player count is: " + room.PlayerCount);
+                OccupancyRateText_ForShooter.text = room.PlayerCount + " / " + 20;
+            }
             else{
-                Debug.Log(room.Name + " didnt match outdoor or school");
+                Debug.Log(room.Name + " didnt match map names");
             }
         }
 
@@ -158,7 +284,6 @@ public class RoomManager : MonoBehaviourPunCallbacks
         roomOptions.MaxPlayers = 20;
 
         string[] roomPropsInLobby = {MultiplayerVRConstants.MAP_TYPE_KEY};
-        // There are 2 maps, "ourdoor" and "school"
 
         ExitGames.Client.Photon.Hashtable customRoomProperties = new ExitGames.Client.Photon.Hashtable() { {MultiplayerVRConstants.MAP_TYPE_KEY, mapType}};
 
