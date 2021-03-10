391fb9b56784ac42a02cef9806e72fb1a5d5c297
Jack Espinosa <jackstar27@gmail.com>
2021-03-10 15:32:03 +1100

    Major basketball greybox and mechanics improvment
    Scoring now works as well as reseting, balls have destroy timer, actual game timer still doesn't work looking for some fixes

--- Assets/Scripts/Basketball/BasketballCollider.cs
+++ Assets/Scripts/Basketball/BasketballCollider.cs
@@ -6,20 +6,56 @@ using TMPro;
 
 public class BasketballCollider : MonoBehaviour
 {
-    // Start is called before the first frame update
-    public static int bballScore;
-    public GameObject bballScoreText;
+    //Score
+    public static int bballScore1;
+    public static int bballScore2;
+    public static int bballScore3;
+    public static int bballScore4;
+
+    //Score Text
+    public Text bballScoreText1;
+    public Text bballScoreText2;
+    public Text bballScoreText3;
+    public Text bballScoreText4;
+
+    //Machine Check
+    public bool Machine1 = false;
+    public bool Machine2 = false;
+    public bool Machine3 = false;
+    public bool Machine4 = false;
 
     void Update()
     {
         //bballScoreText.GetComponent<TMPro.TextMeshPro>().text = "Score: " + bballScore;
-        bballScoreText.GetComponent<Text>().text = "Score: " + bballScore;
+        bballScoreText1.text = bballScore1.ToString();
+        bballScoreText2.text = bballScore2.ToString();
+        bballScoreText3.text = bballScore3.ToString();
+        bballScoreText4.text = bballScore4.ToString();
     }
+
 
     private void OnTriggerEnter(Collider other)
     {
-        Debug.Log("Triggered");
-        bballScore = bballScore + 1;
+        if (Machine1 == true)
+        {
+            Debug.Log("Machine1 score Point");
+            bballScore1 = bballScore1 + 1;
+        }
+        else if (Machine2 == true)
+        {
+            Debug.Log("Machine2 score Point");
+            bballScore2 = bballScore2 + 1;
+        }
+        else if (Machine3 == true)
+        {
+            Debug.Log("Machine3 score Point");
+            bballScore3 = bballScore3 + 1;
+        }
+        else if (Machine4 == true)
+        {
+            Debug.Log("Machine4 score Point");
+            bballScore4 = bballScore4 + 1;
+        }
 
     }
 }
--- Assets/Scripts/Basketball/BasketballManager.cs
+++ Assets/Scripts/Basketball/BasketballManager.cs
@@ -1,18 +1,46 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
 public class BasketballManager : MonoBehaviour
 {
     //Basketballs
-    public GameObject basketBall1;
-    public GameObject basketBall2;
-    public GameObject basketBall3;
+    public GameObject basketBall;
+        
+    
+
+    public int numberOfBalls = 0;
+    
+    //Time Stuff
+    public GameObject timeLeftText1;
+    public GameObject timeLeftText2;
+    public GameObject timeLeftText3;
+    public GameObject timeLeftText4;
+
+    public static float timeLeft1 = 60;
+    public static float timeLeft2 = 60;
+    public static float timeLeft3 = 60;
+    public static float timeLeft4 = 60;
+
     //Int
     public int waitTime = 3;
-    
-    public GameObject StartButton;
-    public GameObject ballBarrier;
+    //Start Buttons
+    public GameObject StartButton1;
+    public GameObject StartButton2;
+    public GameObject StartButton3;
+    public GameObject StartButton4;
+
+    public bool Machine1 { get; }
+    public bool Machine2 { get; }
+    public bool Machine3 { get; }
+    public bool Machine4 { get; }
+
+    //Reset Buttons
+
+
+    //Machine booleans
+
+
     void Start()
     {
         
@@ -21,51 +49,193 @@ public class BasketballManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        timeLeftText1.GetComponent<Text>().text = timeLeft1.ToString("F0");
+        timeLeftText2.GetComponent<Text>().text = timeLeft1.ToString("F0");
+        timeLeftText3.GetComponent<Text>().text = timeLeft1.ToString("F0");
+        timeLeftText4.GetComponent<Text>().text = timeLeft1.ToString("F0");
     }
-
     private Vector3 Position1()
     {
-        float x = -6.9f;
-        float y = 2;
-        float z = 1.6f;
+        float x = -8.3f;
+        float y = 2.1f;
+        float z = -5.8f;
         return new Vector3(x, y, z);
     }
     private Vector3 Position2()
     {
-        float x = -6.9f;
-        float y = 2;
-        float z = 2;
+        float x = -8.3f;
+        float y = 2.1f;
+        float z = -1.848f;
         return new Vector3(x, y, z);
     }
     private Vector3 Position3()
     {
-        float x = -6.9f;
-        float y = 2;
-        float z = 2.6f;
+        float x = -8.3f;
+        float y = 2.1f;
+        float z = 2.053f;
         return new Vector3(x, y, z);
     }
 
-    public void OnStartButtonClicked()
+    private Vector3 Position4()
     {
-        Debug.Log("Start Button Clicked, spawning basketballs");
-        StartButton.SetActive(false);
-        Instantiate(basketBall1, Position1(), Quaternion.identity);
-        Instantiate(basketBall2, Position2(), Quaternion.identity);
-        Instantiate(basketBall3, Position3(), Quaternion.identity);
-        ballBarrier.SetActive(false);
-        
+        float x = -8.3f;
+        float y = 2.1f;
+        float z = 6.075f;
+        return new Vector3(x, y, z);
     }
 
-    public void OnResetButtonClicked()
+    //public void OnStartButtonClicked()
+    //{
+    //    MachineCheck();
+    //}
+
+    //public void MachineCheck()
+    //{
+    //    if (GameObject.Find("ScoreTrigger1").GetComponent<BasketballManager>().Machine1 == true)
+    //    {
+    //        Machine1Spawn();
+
+    //        //Instantiate(basketBall1, Position1(), Quaternion.identity);
+    //        //Instantiate(basketBall2, Position2(), Quaternion.identity);
+    //        //Instantiate(basketBall3, Position3(), Quaternion.identity);
+    //    }
+    //    else if (GameObject.Find("ScoreTrigger1").GetComponent<BasketballManager>().Machine2 == true)
+    //    {
+    //        Machine1Spawn();
+    //    }
+    //    else if (GameObject.Find("ScoreTrigger1").GetComponent<BasketballManager>().Machine3 == true)
+    //    {
+    //        Machine1Spawn();
+    //    }
+    //    else if (GameObject.Find("ScoreTrigger1").GetComponent<BasketballManager>().Machine4 == true)
+    //    {
+    //        Machine1Spawn();
+    //    }
+    //}
+
+    public void Machine1Spawn()
+    {
+        Debug.Log("Start Button Clicked, spawning basketballs");
+        StartButton1.SetActive(false);
+
+        for (int i = 0; i < numberOfBalls; i++)
+        {
+            Instantiate(basketBall, Position1(), Quaternion.identity);
+            Debug.Log("Spawned Basketball in machine 1");
+        }
+
+        if (timeLeft1 > 0)
+        {
+            timeLeft1 = timeLeft1 -= Time.deltaTime;
+            timeLeftText1.GetComponent<Text>().text = timeLeft1.ToString("F0");
+        }
+        else if(timeLeft1 <= 0)
+        {
+            Machine1Reset();
+        }
+    }
+    public void Machine2Spawn()
+    {
+        Debug.Log("Start Button Clicked, spawning basketballs");
+        StartButton2.SetActive(false);
+
+        for (int i = 0; i < numberOfBalls; i++)
+        {
+            Instantiate(basketBall, Position2(), Quaternion.identity);
+            Debug.Log("Spawned Basketball in machine 2");
+        }
+
+        if (timeLeft2 > 0)
+        {
+            timeLeft2 = timeLeft2 -= Time.deltaTime;
+            timeLeftText1.GetComponent<Text>().text = timeLeft2.ToString("F0");
+        }
+        else if (timeLeft2 <= 0)
+        {
+            Machine2Reset();
+        }
+    }
+    public void Machine3Spawn()
+    {
+        Debug.Log("Start Button Clicked, spawning basketballs");
+        StartButton3.SetActive(false);
+
+        for (int i = 0; i < numberOfBalls; i++)
+        {
+            Instantiate(basketBall, Position3(), Quaternion.identity);
+            Debug.Log("Spawned Basketball in machine 3");
+        }
+
+        if (timeLeft3 > 0)
+        {
+            timeLeft3 = timeLeft3 -= Time.deltaTime;
+            timeLeftText1.GetComponent<Text>().text = timeLeft3.ToString("F0");
+        }
+        else if (timeLeft3 <= 0)
+        {
+            Machine3Reset();
+        }
+    }
+    public void Machine4Spawn()
+    {
+        Debug.Log("Start Button Clicked, spawning basketballs");
+        StartButton4.SetActive(false);
+
+        for (int i = 0; i < numberOfBalls; i++)
+        {
+            Instantiate(basketBall, Position4(), Quaternion.identity);
+            Debug.Log("Spawned Basketball in machine 4");
+        }
+
+        if (timeLeft4 > 0)
+        {
+            timeLeft4 = timeLeft4 -= Time.deltaTime;
+            timeLeftText4.GetComponent<Text>().text = timeLeft4.ToString("F0");
+        }
+        else if (timeLeft4 <= 0)
+        {
+            Machine4Reset();
+        }
+    }
+
+    public void Machine1Reset()
     {
         Debug.Log("Restart button clicked, despawn basketballs");
-        GameObject.Destroy(basketBall1);
-        Destroy(basketBall2);
-        Destroy(basketBall3);
-        StartButton.SetActive(true);
-        BasketballCollider.bballScore = 0;
-        ballBarrier.SetActive(true);
+        Destroy(basketBall.gameObject);
+        StartButton1.SetActive(true);
+        BasketballCollider.bballScore1 = 0;
+        timeLeft1 = 60;
+        timeLeftText1.GetComponent<Text>().text = "00";
+    }
+
+    public void Machine2Reset()
+    {
+        Debug.Log("Restart button clicked, despawn basketballs");
+        Destroy(basketBall.gameObject);
+        StartButton2.SetActive(true);
+        BasketballCollider.bballScore2 = 0;
+        timeLeft2 = 60;
+        timeLeftText2.GetComponent<Text>().text = "00";
+    }
+
+    public void Machine3Reset()
+    {
+        Debug.Log("Restart button clicked, despawn basketballs");
+        Destroy(basketBall.gameObject);
+        StartButton3.SetActive(true);
+        BasketballCollider.bballScore3 = 0;
+        timeLeft3 = 60;
+        timeLeftText3.GetComponent<Text>().text = "00";
+    }
+
+    public void Machine4Reset()
+    {
+        Debug.Log("Restart button clicked, despawn basketballs");
+        Destroy(basketBall.gameObject);
+        StartButton4.SetActive(true);
+        BasketballCollider.bballScore4 = 0;
+        timeLeft4 = 60;
+        timeLeftText4.GetComponent<Text>().text = "00";
     }
 
 }
