b18f737d0329e1e94424d85922c80d9895f82e16
James Siebert <js@media360.com.au>
2021-03-23 17:30:23 +1100

    API Communication update - some ui styles

---
+++ Assets/Scripts/Web API/CreditResponse.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+[System.Serializable]
+public class CreditResponse
+{
+    public int balance;
+    public string playerId;
+    
+    public static CreditResponse CreateFromJSON(string jsonString)
+    {
+        return JsonUtility.FromJson<CreditResponse>(jsonString);
+    }
+    
+    // Given JSON input:
+    // {"name":"Dr Charles","lives":3,"health":0.8}
+    // this example will return a PlayerInfo object with
+    // name == "Dr Charles", lives == 3, and health == 0.8f.
+}
